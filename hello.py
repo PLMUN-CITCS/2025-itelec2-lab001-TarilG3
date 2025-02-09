87998056e13ffddb82e604d4b304eed045f66f1e
William Se <itsmewilliamse@gmail.com>
2025-02-09 20:02:12 +0800

    My first Python program

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello world!")
+
+name = "William Se"
+course = "Bachelor of Science in Information Technology"
+interests_in_programming = "Python and Cloud Computing"
+
+print(f"Name: {name}")
+print(f"Course: {course}")
+print(f"Interests in programming: {interests_in_programming}")
